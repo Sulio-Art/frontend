2c74782c7e94b16be79b90f012f14cb607cc5e9a
M-Jay <73007441+manjeetdular@users.noreply.github.com>
2025-07-23 17:57:54 +0530

    Update profileApi.js

--- src/lib/api/profileApi.js
+++ src/lib/api/profileApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const profileApi = createApi({
   reducerPath: "profileApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000/api",
+    baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL ,
     prepareHeaders: (headers, { getState }) => {
       
       const token = getState().auth.token;
